e2b7c4197138b63f30aa7753c8da4de72ef2055e
Irakli Nadareishvili <irakli@gmail.com>
2012-12-27 19:40:48 +0000

    allow environment overrides in dev mode as well

--- dev_start.sh
+++ dev_start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-export NODE_ENV=development
+if [ ! $NODE_ENV ]; then
+  export NODE_ENV=development
+fi
+
 # Note: hot reloading and clustering don't always work well together so it's recommended to disable clustering in dev
 export NODE_CLUSTERED=0
 export NODE_SERVE_STATIC=1
