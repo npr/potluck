8ee740f9a892f6d423de114e2cb329bf8132e958
Irakli Nadareishvili <irakli@gmail.com>
2012-12-29 20:03:17 +0000

    adding startup script for staging

---
+++ stage_start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export NODE_ENV=stage
+
+# Note: hot reloading and clustering don't always work well together so it's recommended to disable clustering in dev
+export NODE_CLUSTERED=0
+export NODE_SERVE_STATIC=1
+export NODE_HOT_RELOAD=1
+
+
+./start.sh
+
