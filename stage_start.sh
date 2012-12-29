87116015fb315e648f85fc8dcf623758ef38cf9a
Irakli Nadareishvili <irakli@gmail.com>
2012-12-29 20:05:34 +0000

    stage startup script should be run with forever, just like prod one

--- stage_start.sh
+++ stage_start.sh
@@ -5,7 +5,7 @@ export NODE_ENV=stage
 # Note: hot reloading and clustering don't always work well together so it's recommended to disable clustering in dev
 export NODE_CLUSTERED=0
 export NODE_SERVE_STATIC=1
-export NODE_HOT_RELOAD=1
+export NODE_HOT_RELOAD=0
 
 
 ./start.sh
